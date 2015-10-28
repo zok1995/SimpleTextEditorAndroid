c58cd1f6cac95d53da24502b2adaef21e602f78e
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2015-10-28 18:37:06 +0100

    додав контекстне меню для вибору шрифта почав додавати список для типу тексту

--- Text/app/src/main/java/com/example/oleksandr/text/MainActivity.java
+++ Text/app/src/main/java/com/example/oleksandr/text/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.oleksandr.text;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -17,6 +22,14 @@ import java.io.OutputStreamWriter;
 public class MainActivity extends ActionBarActivity {
     private final static String FILENAME = "doit.txt";
     private EditText mEditTextMain;
+    private Button mButtonTextSize;
+    private Spinner mSpinnerTypeText;
+
+    final int MENU_SIZE_22 = 4;
+    final int MENU_SIZE_26 = 5;
+    final int MENU_SIZE_30 = 6;
+    final int MENU_SIZE_34 = 7;
+    final int MENU_SIZE_38 = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mEditTextMain = (EditText) findViewById(R.id.editTextMain);
+
+        mButtonTextSize = (Button) findViewById(R.id.buttonTextSize);
+
+        mSpinnerTypeText = (Spinner) findViewById(R.id.spinnerTypeText);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerTypeText, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        mSpinnerTypeText.setAdapter(adapter);
+
+        registerForContextMenu(mButtonTextSize);
     }
 
     @Override
@@ -31,6 +54,43 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()){
+            case R.id.buttonTextSize:
+                menu.add(0,MENU_SIZE_22,0, "22");
+                menu.add(0,MENU_SIZE_26, 0, "26");
+                menu.add(0,MENU_SIZE_30, 0, "30");
+                menu.add(0,MENU_SIZE_34, 0, "34");
+                menu.add(0,MENU_SIZE_38, 0, "38");
+                break;
+
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case MENU_SIZE_22:
+                mEditTextMain.setTextSize(22);
+                break;
+            case MENU_SIZE_26:
+                mEditTextMain.setTextSize(26);
+                break;
+            case MENU_SIZE_30:
+                mEditTextMain.setTextSize(30);
+                break;
+            case MENU_SIZE_34:
+                mEditTextMain.setTextSize(34);
+                break;
+            case MENU_SIZE_38:
+                mEditTextMain.setTextSize(38);
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
